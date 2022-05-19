079791edc0b655c2a20168142810058b51a3924f
kevin <kelvinotix@gmail.com>
2022-05-20 02:10:02 +0300

    connecting frontend to backend

---
+++ mern-chat-backend/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@nodetuts.6tosh.mongodb.net/chatAppMern?retryWrites=true&w=majority'`),
+console.log('connected to mongodb')
+// mongoose
+//   .connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("DB Connetion Successfull");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
