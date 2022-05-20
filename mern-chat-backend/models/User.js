77849b3be9782ebfc8474d331655c550cb9484e8
kevin <kelvinotix@gmail.com>
2022-05-20 15:36:50 +0300

    successful connection to the chat via login, user was created successfully

--- mern-chat-backend/models/User.js
+++ mern-chat-backend/models/User.js
@@ -38,13 +38,13 @@ UserSchema.pre('save', function(){
     const user =  this
     if(!user.isModified('password')) return next();
     bcrypt.genSalt(10, function(err,salt){
-        if(err) return next(err)
+        if(err) return next(err);
 
         bcrypt.hash(user.password, salt, function(err,hash){
-            if (err) return next(err)
+            if (err) return next(err);
 
             user.password = hash
-            next();
+            // next();
         })
     })
 })
